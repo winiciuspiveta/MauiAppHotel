a996774ecdde8583a4f0f9cc38bd3f66c6c2d3d6
winiciuspiveta <winiciusfbp@gmail.com>
2025-10-02 19:09:38 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+
+            var window = base.CreateWindow(activationState);
+
+            window.Height = 600;
+            window.Width = 400;
+
+            return window;
+        } // Essa parte mexe apenas com as dimensões da janela 
+    }
+}
