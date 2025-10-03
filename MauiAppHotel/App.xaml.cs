c5705f07f831ddbd53f3a1c324ec270ef411f6e4
winiciuspiveta <winiciusfbp@gmail.com>
2025-10-03 15:03:44 -0300

    App Hotel Finalizado

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,42 @@
 ﻿
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto> // aqui eu puxo os objetos da classe QUARTO
+        {
+            new Quarto()
+            {
+                 Descricao = "Suíte Super Luxo",
+                 ValorDiariaAdulto = 110.0,
+                 ValorDiariaCrianca = 55.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            }, 
+
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.50
+            }
+
+        };
+
         public App()
         {
             InitializeComponent();
--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -6,4 +6,17 @@ public partial class HospedagemContratada : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PopAsync(); // quando eu navego por uma página e desejo voltar, uso PopAsync
+
+
+		} catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
